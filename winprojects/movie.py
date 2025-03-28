be35dd28e018d4f08a3ae0ebdab42a60167ec1a9
geegee1977 <graemegrant77@gmail.com>
2025-03-28 15:10:36 +0000

    commit

--- winprojects/movie.py
+++ winprojects/movie.py
@@ -2,5 +2,12 @@ current_movies={'Jaws':'18:30',
                'slasher':'17:30',
                'goonies':'20:00'}
 
-print(current_movies)
+print('The current mvoies are')
+for yu in current_movies:
+    print(yu)
+
+user_choice = input('enter the movie name:')
+                    
+
+print(current_movies[user_choice])
 
